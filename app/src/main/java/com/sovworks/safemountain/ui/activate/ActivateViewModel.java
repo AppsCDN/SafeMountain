4b7f8cc137b60169a4a2aff6799c44fac183702b
kigael <kigael16@naver.com>
2020-04-10 08:05:18 +0900

    Bye sovwork
    BYEBYE

--- app/src/main/java/com/sovworks/safemountain/ui/activate/ActivateViewModel.java
+++
@@ -1,9 +0,0 @@
-package com.sovworks.safemountain.ui.activate;
-
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
-public class ActivateViewModel extends ViewModel {
-
-}
